98ab776074ec57a1882965cbf2df61e1765b5ba0
Mark Howells-Mead <permanent.tourist@gmail.com>
2016-03-09 15:54:05 +0100

    jQuery toggleClicker

---
+++ javascript/jquery.toggleClicker.js
@@ -0,0 +1,21 @@
+(function($,undefined){
+	$.fn.extend({
+		toggleClicker: function() {
+            $(this).off('click.toggled').on('click.toggled', function(e){
+                if( $(this).data('target') ){
+                    e.preventDefault();
+                    $(this).toggleClass('is-open');
+                    var $target = $($(this).data('target'));
+                    $target.toggleClass('is-closed');
+                    if( !$target.hasClass('is-closed') ){
+                        var destination = $target.offset().top - anchorAnimateOffset;
+                        setTimeout(function(){
+                		    $('html:not(:animated),body:not(:animated)').animate({ scrollTop: destination}, 600);
+                        }, 300);
+                    }
+                }
+            });
+			return this;
+		}
+	});
+})(jQuery);
